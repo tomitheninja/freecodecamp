43afc8fdb17ee6fb5dcf576d20ebde66ae7c45c7
Südi Tamás <suditomi@pm.me>
2019-12-11 10:22:39 +0000

    Update 6 sum square difference.js

--- project-euler/6 sum square difference.js
+++ project-euler/6 sum square difference.js
@@ -1,10 +1,12 @@
 function sumSquareDifference(n) {
-  let sum1 = 0, sum2 = 0
-  for (let i = 1; i <= n; i++) {
-    sum1 += i
-    sum2 += i ** 2
-  }
-  sum1 = sum1 ** 2
+  let sum1 = ((n + 1) * n / 2) ** 2
+  let sum2 = n * (n + 1) * (2*n + 1) / 6
+  // let sum1 = 0, sum2 = 0
+  // for (let i = 1; i <= n; i++) {
+  //   sum1 += i
+  //   sum2 += i ** 2
+  // }
+  // sum1 = sum1 ** 2
   return sum1 - sum2
 }
 
