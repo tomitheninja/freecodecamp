c32fff6197ee550fa8f7298a24b1a43a00f79f09
Südi Tamás <suditomi@pm.me>
2019-12-11 09:47:32 +0000

    Update project-euler/6 sum square difference.js

---
+++ project-euler/6 sum square difference.js
@@ -0,0 +1,11 @@
+function sumSquareDifference(n) {
+  let sum1 = 0, sum2 = 0
+  for (let i = 1; i <= n; i++) {
+    sum1 += i
+    sum2 += i ** 2
+  }
+  sum1 = sum1 ** 2
+  return sum1 - sum2
+}
+
+sumSquareDifference(100);
